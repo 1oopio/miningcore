5b2a04392a9d3b156361e46d91200279183251b8
Philipp Andreas <github@smurfy.de>
2022-08-01 23:44:39 +0200

    feat(#36): Started KASPA support, basic classes and grpc support for node communication

---
+++ src/Miningcore/Blockchain/Kaspa/KaspaJob.cs
@@ -0,0 +1,21 @@
+using Miningcore.Blockchain.Cryptonote.DaemonResponses;
+using Miningcore.Configuration;
+using Miningcore.Extensions;
+using Miningcore.Native;
+using Miningcore.Stratum;
+using Miningcore.Util;
+using Org.BouncyCastle.Math;
+using static Miningcore.Native.Cryptonight.Algorithm;
+using Contract = Miningcore.Contracts.Contract;
+
+namespace Miningcore.Blockchain.Kaspa;
+
+public class KaspaJob
+{
+
+    #region API-Surface
+
+      
+
+    #endregion // API-Surface
+}
