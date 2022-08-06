c7ff835cd84c8f72134b4cd7974441786b3b660f
Philipp Andreas <github@smurfy.de>
2022-08-06 02:15:17 +0200

    feat(#36): basic stratum support, get template support

--- src/Miningcore/Blockchain/Kaspa/KaspaJob.cs
+++ src/Miningcore/Blockchain/Kaspa/KaspaJob.cs
@@ -1,21 +1,117 @@
-using Miningcore.Blockchain.Cryptonote.DaemonResponses;
-using Miningcore.Configuration;
 using Miningcore.Extensions;
-using Miningcore.Native;
-using Miningcore.Stratum;
-using Miningcore.Util;
-using Org.BouncyCastle.Math;
-using static Miningcore.Native.Cryptonight.Algorithm;
+using System.Text;
+using Miningcore.Crypto.Hashing.Algorithms;
 using Contract = Miningcore.Contracts.Contract;
+using Miningcore.Blockchain.Kaspa.RPC;
+using BigInteger = System.Numerics.BigInteger;
 
 namespace Miningcore.Blockchain.Kaspa;
 
 public class KaspaJob
 {
+    public KaspaJob(RpcBlock block, string jobId, string prevHash)
+    {
+        Contract.RequiresNonNull(block);
+        Contract.Requires<ArgumentException>(!string.IsNullOrEmpty(jobId));
+
+        Block = block;
+        PrevHash = prevHash;
+    }
+
+    private static readonly Blake2b hasher = new Blake2b();
 
     #region API-Surface
 
-      
+
+    public string PrevHash { get; }
+    public RpcBlock Block { get; }
+
+    public void PrepareWorkerJob(KaspaWorkerJob workerJob, out BigInteger[] jobs, out long timestamp)
+    {
+        timestamp = Block.Header.Timestamp;
+        jobs = JobData(PrevHash);
+    }
+
+    public static string HashBlock(RpcBlock block, Boolean prePow)
+    {
+        IntPtr blakeState = hasher.InitKey(32, Encoding.ASCII.GetBytes("BlockHash"));
+
+        {
+            Span<byte> data = stackalloc byte[10];
+            BitConverter.GetBytes(((ushort) block.Header.Version)).CopyTo(data[0..]);
+            BitConverter.GetBytes((UInt64) block.Header.Parents.Count).CopyTo(data[2..]);
+            hasher.Update(blakeState, data);
+        }
+
+        foreach(var parent in block.Header.Parents)
+        {
+            hasher.Update(blakeState, BitConverter.GetBytes((UInt64) parent.ParentHashes.Count));
+            foreach(var parentHash in parent.ParentHashes)
+            {
+                hasher.Update(blakeState, parentHash.HexToByteArray());
+            }
+        }
+
+        hasher.Update(blakeState, block.Header.HashMerkleRoot.HexToByteArray());
+        hasher.Update(blakeState, block.Header.AcceptedIdMerkleRoot.HexToByteArray());
+        hasher.Update(blakeState, block.Header.UtxoCommitment.HexToByteArray());
+
+        {
+            Span<byte> data = stackalloc byte[36];
+            BitConverter.GetBytes(((UInt64) (prePow ? 0 : block.Header.Timestamp))).CopyTo(data[0..]);
+            BitConverter.GetBytes((UInt32) block.Header.Bits).CopyTo(data[8..]);
+            BitConverter.GetBytes(((UInt64) (prePow ? 0 : block.Header.Nonce))).CopyTo(data[12..]);
+            BitConverter.GetBytes(((UInt64) block.Header.DaaScore)).CopyTo(data[20..]);
+            BitConverter.GetBytes(((UInt64) block.Header.BlueScore)).CopyTo(data[28..]);
+            hasher.Update(blakeState, data);
+        }
+
+        var blueWork = block.Header.BlueWork;
+        var parsedBlueWork = blueWork.PadLeft(blueWork.Count() + blueWork.Count() % 2, '0').HexToByteArray();
+
+        hasher.Update(blakeState, BitConverter.GetBytes((UInt64) parsedBlueWork.Count()));
+        hasher.Update(blakeState, parsedBlueWork);
+                
+        hasher.Update(blakeState, block.Header.PruningPoint.HexToByteArray());
+
+        Span<byte> hash = stackalloc byte[32];
+        hasher.Final(blakeState, hash);
+
+        return hash.ToHexString();
+    }
+
+    public static BigInteger[] JobData(string hash)
+    {
+        var hashData = hash.HexToByteArray().AsSpan();
+        List<BigInteger> preHashU64s = new List<BigInteger>();
+
+        for(var i = 0; i < 4; i++)
+        {
+            var data = hashData.Slice(i * 8, 8);
+            preHashU64s.Add(data.ToBigInteger());
+        }
+
+        return preHashU64s.ToArray();
+    }
+
+    public Double EncodeTarget()
+    {
+        var bits = new BigInteger(Block.Header.Bits);
+        Int32 unshiftedExpt = (Int32) (bits >> 24);
+        var mant = bits & 1850408; //FFFFFF
+        Int32 expt = 0;
+
+        if (unshiftedExpt <= new BigInteger(3))
+        {
+            mant = mant >> (8 * (3 - unshiftedExpt));
+            expt = 0;
+        } else
+        {
+            expt = (Int32) (8 * ((bits >> 24) - 3));
+        }
+        return (Math.Pow(2, 255) / ((double)(mant << expt))) / Math.Pow(2, 31);
+    }
+
 
     #endregion // API-Surface
 }
