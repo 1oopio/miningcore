6b19af137113f609f5d5905ccb21d4ad61e527fa
Philipp Andreas <github@smurfy.de>
2022-08-07 15:23:04 +0200

    feat(#36): started with wallet/payout support

--- src/Miningcore/Blockchain/Kaspa/KaspaConstants.cs
+++ src/Miningcore/Blockchain/Kaspa/KaspaConstants.cs
@@ -7,12 +7,12 @@ public enum KaspaNetworkType
 {
     Main = 1,
     Test,
-    Dev,
-    Sim
+    Dev
 }
 
 public static class KaspaConstants
 {
+    public const string WalletDaemonCategory = "wallet";
     public static readonly BigInteger Diff1 = BigInteger.Parse("00ffff0000000000000000000000000000000000000000000000000000", NumberStyles.HexNumber);
 #if DEBUG
     public const int PayoutMinBlockConfirmations = 2;
---
+++ src/Miningcore/Blockchain/Kaspa/RPC/KaspaGrpcWalletClient.cs
@@ -0,0 +1,117 @@
+using Grpc.Net.Client;
+using Grpc.Core;
+using System.Diagnostics;
+using Miningcore.Configuration;
+using Miningcore.Extensions;
+using Miningcore.Messaging;
+using Miningcore.Notifications.Messages;
+using NLog;
+using Contract = Miningcore.Contracts.Contract;
+using Miningcore.Blockchain.Kaspa.RPC.Wallet;
+
+
+namespace Miningcore.Blockchain.Kaspa.RPC;
+public class KaspaGrpcWalletClient
+{
+    public KaspaGrpcWalletClient(DaemonEndpointConfig endPoint, IMessageBus messageBus, string poolId)
+    {
+        Contract.RequiresNonNull(messageBus);
+        Contract.Requires<ArgumentException>(!string.IsNullOrEmpty(poolId));
+
+        config = endPoint;
+        this.messageBus = messageBus;
+        this.poolId = poolId;
+    }
+
+    protected readonly DaemonEndpointConfig config;
+    private readonly IMessageBus messageBus;
+    private readonly string poolId;
+
+    private kaspawalletd.kaspawalletdClient GetClient()
+    {
+        var protocol = config.Ssl ? Uri.UriSchemeHttps : Uri.UriSchemeHttp;
+        var requestUrl = $"{protocol}://{config.Host}:{config.Port}";
+        var channel = GrpcChannel.ForAddress(requestUrl);
+        return new kaspawalletd.kaspawalletdClient(channel);
+    }
+
+    public async Task<GetBalanceResponse> GetBalanceAsync(ILogger logger, CancellationToken ct)
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            var client = GetClient();
+            var req = new GetBalanceRequest();
+
+            logger.Trace(() => $"Sending gRPC request to wallet: {req}");
+
+            var response = await client.GetBalanceAsync(req, null, null, ct);
+
+            logger.Trace(() => $"Received gRPC response: {response}");
+
+            messageBus.SendTelemetry(poolId, TelemetryCategory.RpcRequest, "GetBalanceAsync", sw.Elapsed, response != null);
+            return response;
+        }
+        catch(TaskCanceledException)
+        {
+            return null;
+        }
+        catch(Exception ex)
+        {
+            return null;
+        }
+    }
+
+    public async Task<ShowAddressesResponse> ShowAddressesAsync(ILogger logger, CancellationToken ct)
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            var client = GetClient();
+            var req = new ShowAddressesRequest();
+
+            logger.Trace(() => $"Sending gRPC request to wallet: {req}");
+
+            var response = await client.ShowAddressesAsync(req, null, null, ct);
+
+            logger.Trace(() => $"Received gRPC response: {response}");
+
+            messageBus.SendTelemetry(poolId, TelemetryCategory.RpcRequest, "ShowAddressesAsync", sw.Elapsed, response != null);
+            return response;
+        }
+        catch(TaskCanceledException)
+        {
+            return null;
+        }
+        catch(Exception ex)
+        {
+            return null;
+        }
+    }
+
+    public async Task<SendResponse> SendAsync(ILogger logger, SendRequest req, CancellationToken ct)
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            var client = GetClient();
+
+            logger.Trace(() => $"Sending gRPC request to wallet: {req}");
+
+            var response = await client.SendAsync(req, null, null, ct);
+
+            logger.Trace(() => $"Received gRPC response: {response}");
+
+            messageBus.SendTelemetry(poolId, TelemetryCategory.RpcRequest, "SendAsync", sw.Elapsed, response != null);
+            return response;
+        }
+        catch(TaskCanceledException)
+        {
+            return null;
+        }
+        catch(Exception ex)
+        {
+            return null;
+        }
+    }
+}
