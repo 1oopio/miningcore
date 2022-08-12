cf168b807466f2276b661821475976b1aab60d56
Philipp Andreas <github@smurfy.de>
2022-08-13 01:46:01 +0200

    feat(#36): getting payouts working, tried to optimize block reward calculation

---
+++ src/Miningcore/Blockchain/Kaspa/Configuration/KaspaPoolConfigExtra.cs
@@ -0,0 +1,11 @@
+using Miningcore.Configuration;
+
+namespace Miningcore.Blockchain.Kaspa.Configuration;
+
+public class KaspaPoolConfigExtra
+{
+    /// <summary>
+    /// Additional payload for block template
+    /// </summary>
+    public string BlockTemplatePayload { get; set; }
+}
