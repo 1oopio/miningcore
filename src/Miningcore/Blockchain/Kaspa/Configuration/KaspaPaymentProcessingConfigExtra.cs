911392fbdc42bda033b677d3cad303f4f62e5d0a
Philipp Andreas <github@smurfy.de>
2022-08-13 01:46:01 +0200

    feat(#36): getting payouts working, tried to optimize block reward calculation

---
+++ src/Miningcore/Blockchain/Kaspa/Configuration/KaspaPaymentProcessingConfigExtra.cs
@@ -0,0 +1,9 @@
+namespace Miningcore.Blockchain.Kaspa.Configuration;
+
+public class KaspaPaymentProcessingConfigExtra
+{
+    /// <summary>
+    /// Password for sending coins
+    /// </summary>
+    public string WalletPassword { get; set; }
+}
