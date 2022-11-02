2b22c87dce144b78fdeb2b54b215560f835e290a
LoulouCrypto <loulou@louloucrypto.fr>
2022-11-02 19:19:47 +0100

    Test Add

--- src/Miningcore/Blockchain/Ethereum/EthereumPayoutHandler.cs
+++ src/Miningcore/Blockchain/Ethereum/EthereumPayoutHandler.cs
@@ -405,7 +405,7 @@ public class EthereumPayoutHandler : PayoutHandlerBase,
 
         var amount = (BigInteger) Math.Floor(balance.Amount * EthereumConstants.Wei);
 
-        SendTransactionRequest request = new SendTransactionRequest
+        var request = new SendTransactionRequest
         {
             From = poolConfig.Address,
             To = balance.Address,
