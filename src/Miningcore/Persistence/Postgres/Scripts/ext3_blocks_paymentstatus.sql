e1ecd0026020d47756461819b6974c94a6e4380b
jon4hz <me@jon4hz.io>
2022-11-20 21:34:43 +0100

    fix: sql update statement

--- src/Miningcore/Persistence/Postgres/Scripts/ext3_blocks_paymentstatus.sql
+++ src/Miningcore/Persistence/Postgres/Scripts/ext3_blocks_paymentstatus.sql
@@ -1,3 +1,15 @@
-ALTER TABLE blocks ADD COLUMN paymentstatus TEXT NOT NULL;
+ALTER TABLE blocks ADD COLUMN paymentstatus TEXT NULL;
+
+UPDATE blocks
+SET paymentstatus = (
+    CASE "status"
+        WHEN 'confirmed' THEN 'paid'
+        WHEN 'orphaned' THEN 'orphaned'
+        ELSE 'pending'
+    END
+)
+WHERE paymentstatus IS NULL;
+
+ALTER TABLE blocks ALTER COLUMN paymentstatus SET NOT NULL;
 
 CREATE INDEX IDX_BLOCKS_POOL_BLOCK_STATUS_PAYMENTSTATUS on blocks(poolid, status, paymentstatus);
