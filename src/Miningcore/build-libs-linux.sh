ce4c99fec70ad08fb7dc275bd88115ade6712003
jon4hz <me@jon4hz.io>
2022-11-25 19:30:13 +0100

    chore: print CPU features when building (#121)

--- src/Miningcore/build-libs-linux.sh
+++ src/Miningcore/build-libs-linux.sh
@@ -12,6 +12,8 @@ AVX512F=$(../Native/check_cpu.sh avx512f && echo -mavx512f || echo)
 
 export CPU_FLAGS="$AES $SSE2 $SSE3 $SSSE3 $AVX $AVX2 $AVX512F"
 
+echo "CPU_FLAGS=$CPU_FLAGS"
+
 HAVE_AES=$(../Native/check_cpu.sh aes && echo -D__AES__ || echo)
 HAVE_SSE2=$(../Native/check_cpu.sh sse2 && echo -DHAVE_SSE2 || echo)
 HAVE_SSE3=$(../Native/check_cpu.sh sse3 && echo -DHAVE_SSE3 || echo)
@@ -21,6 +23,8 @@ HAVE_AVX2=$(../Native/check_cpu.sh avx2 && echo -DHAVE_AVX2 || echo)
 HAVE_AVX512F=$(../Native/check_cpu.sh avx512f && echo -DHAVE_AVX512F || echo)
 
 export HAVE_FEATURE="$HAVE_AES $HAVE_SSE2 $HAVE_SSE3 $HAVE_SSSE3 $HAVE_AVX $HAVE_AVX2 $HAVE_AVX512F"
+
+echo "HAVE_FEATURES=$HAVE_FEATURE"
 
 (cd ../Native/libmultihash && make clean && make -j) && mv ../Native/libmultihash/libmultihash.so "$OutDir"
 (cd ../Native/libethhash && make clean && make -j) && mv ../Native/libethhash/libethhash.so "$OutDir"
